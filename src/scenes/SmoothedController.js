6cd7a505cf336dc944fc94413d88104a03f6b35a
Nathan Altice <naltice@ucsc.edu>
2020-05-08 12:57:38 -0700

    removed debug log

--- src/scenes/SmoothedController.js
+++ src/scenes/SmoothedController.js
@@ -43,7 +43,7 @@ class SmoothedController extends Phaser.Scene {
         this.cameras.main.setZoom(0.5);
 
         // DEBUG
-        console.log(this.cameras);
+        //console.log(this.cameras);
 
     }
 
